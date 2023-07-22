924cd7eb35bbb83649e0513531e9d6ffe85a2d36
Joel <bacierto.joel@gmail.com>
2023-07-22 13:02:44 +0800

    Added theming on the app

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -11,31 +11,32 @@ class ExpenseItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 16,
-          ),
-          child: Column(
-            children: <Widget>[
-              Text(expense.title),
-              const SizedBox(height: 4),
-              Row(
-                children: <Widget>[
-                  Text(
-                    '₱ ${expense.amount.toStringAsFixed(2)}',
-                  ),
-                  const Spacer(),
-                  Row(
-                    children: <Widget>[
-                      Icon(categoryIcons[expense.category]),
-                      const SizedBox(width: 8),
-                      Text(expense.formattedDate),
-                    ],
-                  ),
-                ],
-              )
-            ],
-          )),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 16,
+        ),
+        child: Column(
+          children: <Widget>[
+            Text(expense.title),
+            const SizedBox(height: 4),
+            Row(
+              children: <Widget>[
+                Text(
+                  '₱ ${expense.amount.toStringAsFixed(2)}',
+                ),
+                const Spacer(),
+                Row(
+                  children: <Widget>[
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(width: 8),
+                    Text(expense.formattedDate),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
